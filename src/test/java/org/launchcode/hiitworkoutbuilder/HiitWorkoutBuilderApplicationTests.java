a21353f05d35c0fe67cbc4424744d5609c007745
cgeffre <cgeffre@gmail.com>
2021-12-01 16:12:11 -0500

    initial spring project commit

---
+++ src/test/java/org/launchcode/hiitworkoutbuilder/HiitWorkoutBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.hiitworkoutbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiitWorkoutBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
