09dd27d3148c89628891b82201c76372dfb98f22
cgeffre <cgeffre@gmail.com>
2021-12-06 21:03:45 -0500

    initialized workout view and getmapping based on workoutId

--- src/main/java/org/launchcode/hiitworkoutbuilder/controllers/SelectWorkoutController.java
+++ src/main/java/org/launchcode/hiitworkoutbuilder/controllers/SelectWorkoutController.java
@@ -1,6 +1,7 @@
 package org.launchcode.hiitworkoutbuilder.controllers;
 
 import org.launchcode.hiitworkoutbuilder.models.Workout;
+import org.launchcode.hiitworkoutbuilder.models.data.ExerciseRepository;
 import org.launchcode.hiitworkoutbuilder.models.data.WorkoutRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("select")
@@ -28,5 +31,12 @@ public class SelectWorkoutController {
         model.addAttribute("workout", workout);
         model.addAttribute("exercises", workout.getExercises());
         return "select/view";
+    }
+
+    @GetMapping("workout/{workoutId}")
+    public String doWorkout(Model model, @PathVariable int workoutId) {
+        Workout workout = workoutRepository.findById(workoutId).orElse(new Workout());
+        model.addAttribute("workout", workout);
+        return "select/workout";
     }
 }
