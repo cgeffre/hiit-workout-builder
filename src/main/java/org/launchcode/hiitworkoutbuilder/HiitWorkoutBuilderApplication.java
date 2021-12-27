3565dca08fe280bce8fe03dd4fc59b689c68e97b
cgeffre <cgeffre@gmail.com>
2021-12-27 18:00:41 -0500

    conversion to war

--- src/main/java/org/launchcode/hiitworkoutbuilder/HiitWorkoutBuilderApplication.java
+++ src/main/java/org/launchcode/hiitworkoutbuilder/HiitWorkoutBuilderApplication.java
@@ -2,9 +2,16 @@ package org.launchcode.hiitworkoutbuilder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HiitWorkoutBuilderApplication {
+public class HiitWorkoutBuilderApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(HiitWorkoutBuilderApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HiitWorkoutBuilderApplication.class, args);
