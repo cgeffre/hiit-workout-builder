673645770e98a2f9ad19eb177957ba926fd69b48
cgeffre <cgeffre@gmail.com>
2021-12-26 19:42:56 -0500

    removed custom equality check to fix indexOf issue with rests when running workouts

--- src/main/java/org/launchcode/hiitworkoutbuilder/models/AbstractEntity.java
+++ src/main/java/org/launchcode/hiitworkoutbuilder/models/AbstractEntity.java
@@ -35,17 +35,4 @@ public abstract class AbstractEntity {
         return name;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        AbstractEntity that = (AbstractEntity) o;
-        return id == that.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
 }
